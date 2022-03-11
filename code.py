d6112a40fa015c855da0ca1f61cee2251705b820
mahammoud <mohammaoud2005@gmail.com>
2022-03-11 19:44:46 +0200

    Fixed data entry

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
-from concurrent.futures import process
 import datetime
 from logging import exception
+import math
+import this
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -10,97 +11,105 @@ import pandas as pa
 import random
 import time
 import os
+import csv
 datarow = []
-colomns = []
-with open('./Input/settings.txt', 'r') as f:
-    try:
-        mainUrl = f.readline()
+with open(r'C:\Users\admin\OneDrive\Documents\GitHub\facebook-scraper\input\settings.txt', 'r') as f:
         chromedriverpath = f.readline()
-    except Exception as e :
-        print('Please provide a valid url or chrome driver path'+e.args[0])
 
-    
-    
 #Just an rng factor for the delay between requests to make the bot more human
 def random_float(low, high):
     return random.random()*(high-low) + low
+
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
 driver = webdriver.Chrome(chromedriverpath,chrome_options=chrome_options)
+
+def doscrape(url,maxdivs):
+    thisrow=[]
+    driver.get(url)
+    try:
+        reviews_and_rating = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".lr9zc1uh.a5q79mjw.g1cxx5fr.b1v8xokw"))).text.split('\n')[2]
+        rating = reviews_and_rating.split('(')[0][0:-1]
+        reviews = reviews_and_rating.split('(')[1][0:-1]
+    except Exception as e:
+        reviews_and_rating = None
+    try:
+        video_text = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR,".lrazzd5p.oo9gr5id.hzawbc8m"))).text
+    except Exception as e:
+        video_text = None
+    try:
+        views_vid= WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh e9vueds3 j5wam9gi b1v8xokw m9osqain']"))).text
+    except Exception as e:
+        views_vid = None
+    title = driver.find_element(By.CLASS_NAME, "embtmqzv").text
+    time.sleep(random_float(0.5,2))
+    driver.get(url+'/about/?ref=page_internal')
+    time.sleep(random_float(0.5,2))
+    alldivs = driver.find_elements(By.CLASS_NAME, 'je60u5p8')
+    thisrow.append(title)
+    if reviews_and_rating != None :
+        thisrow.append(rating)
+        thisrow.append(reviews)
+    else:
+        thisrow.append('NA')
+        thisrow.append('NA')
+    if video_text != None :
+        thisrow.append(video_text)
+    else:
+        thisrow.append('NA')
+    if views_vid != None :
+        thisrow.append(views_vid)
+    else:
+        thisrow.append('NA')
+    currentdivs =0
+    for i in alldivs:
+        isfirst = True
+        firstdiv =""
+        titledivs = i.find_elements(By.XPATH, ".//span[@dir='auto']")
+        currentdivs += len(titledivs)
+        for v in titledivs:
+            if (isfirst):
+                isfirst = False
+                firstdiv = v.text
+            else:
+                if(v.text != "Send message"):
+                    thisrow.append(f'{firstdiv}: {v.text}')
+    maxdivs = max(currentdivs, maxdivs)
+    return thisrow,maxdivs
 try:
     driver.get("https://www.facebook.com")
     username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
     password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
-    time.sleep(random_float(1,3))
     username.clear()
     username.send_keys("majholmeme@gmail.com")
-    time.sleep(random_float(1,3))
     password.clear()
     password.send_keys("mhmdashek445")
-    time.sleep(random_float(1,3))
-    button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-    time.sleep(random_float(1,3))
-    driver.get(mainUrl)
-    divexist = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='ihqw7lf3']")))
-    try:
-        reviews_and_rating = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".lr9zc1uh.a5q79mjw.g1cxx5fr.b1v8xokw"))).text.split('\n')[2]
-        rating = reviews_and_rating.split('(')[0][0:-1]
-        reviews = reviews_and_rating.split('(')[1][0:-1]
-    except Exception as e:
-        reviews_and_rating = None
-    try:  
-        reviews_and_rating = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".lr9zc1uh.a5q79mjw.g1cxx5fr.b1v8xokw"))).text.split('\n')[2]
-        rating = reviews_and_rating.split('(')[0][0:-1]
-        reviews = reviews_and_rating.split('(')[1][0:-1]
-    except Exception as e:
-        reviews_and_rating = None
-    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    try:
-        video_text = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR,".lrazzd5p.oo9gr5id.hzawbc8m"))).text
-    except Exception as e:
-        video_text = None
-    try:
-        views_vid= WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh e9vueds3 j5wam9gi b1v8xokw m9osqain']"))).text
-    except Exception as e:
-        views_vid = None
-    title = driver.find_element(By.CLASS_NAME, "embtmqzv").text
-    time.sleep(random_float(1,3))
-    elements = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='s9t1a10h']")))
-    elements =  driver.find_element(By.XPATH, "//div[@class='s9t1a10h']//span[@dir='auto']")
-    print("created_at")
-    print("===>",elements.get_attribute('innerHTML'))
-    driver.get(mainUrl+'/about/?ref=page_internal')
-    elements = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='je60u5p8']")))
-    alldivs = driver.find_elements(By.CLASS_NAME, 'je60u5p8')
-    for i in alldivs:
-        isfirst = True
-        firstdiv =""
-        space = ""
-        titledivs = i.find_elements(By.XPATH, ".//span[@dir='auto']")
-        for v in titledivs:
-            print(space,v.text)
-            if (isfirst):
-                isfirst = False
-                space = "===>"
-                firstdiv = v.text
-            else:
-                if(v.text != "Send message"):
-                    datarow.append([v.text,firstdiv])
-    if reviews_and_rating != None :
-        datarow.append([rating,'Rating'])
-        datarow.append([reviews, 'Reviews'])
-    if video_text != None :
-        datarow.append([video_text,'Video Text'])
-    if views_vid != None : 
-        datarow.append([views_vid,'views'])
-    datarow.append([title,'Page Name'])
+    time.sleep(random_float(0.5,2))
+    button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+    time.sleep(random_float(0.5,2))
+
 except Exception as e:
     print('error: ',e)
+
+with open(r'.\input\Book1.csv' ,'r') as csv_file:
+    csv_reader= csv.reader(csv_file)
+    maxdivs = 0
+    for line in csv_reader:
+        currentrow, mdivs = doscrape(line[0],maxdivs)
+        datarow.append(currentrow)
+        maxdivs = mdivs
+
 driver.close()
 driver.quit()
-dt_f = pa.DataFrame(data=datarow,columns=['Text','Parent'])
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'OneDrive') + '\Desktop'
-isExist = os.path.exists(desktop)
-csvpath = os.path.join(desktop,str(int(round(datetime.datetime.timestamp(datetime.datetime.now())))))+'.csv'
+for row in datarow: 
+    while (maxdivs + 1 - len(row)) > 0 :
+        row.append('NA')
+datacolumns = ['Page Title','Rating','Reviews','Video Text','Video Views']
+while len(datacolumns) < maxdivs+1:
+    datacolumns.append(f'About#{len(datacolumns)- 4}')
+dt_f = pa.DataFrame(data=datarow,columns=datacolumns)
+dest = '.\Output'
+isExist = os.path.exists(dest)
+csvpath = os.path.join(dest,str(int(round(datetime.datetime.timestamp(datetime.datetime.now())))))+'.csv'
 dt_f.to_csv(csvpath,index=False)
